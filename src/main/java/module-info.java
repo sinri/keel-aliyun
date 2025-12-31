8b30d01245e55d50de0e988543010f9159ace491
sinri <e.joshua.s.e@gmail.com>
2025-12-31 18:05:43 +0800

    build: mark dependencies as transitive and bump version to `5.0.0-rc.22`
    Signed-off-by: sinri <e.joshua.s.e@gmail.com>

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,12 +1,12 @@
 module io.github.sinri.keel.integration.aliyun {
     requires com.google.protobuf;
-    requires io.github.sinri.keel.base;
-    requires io.github.sinri.keel.logger.api;
-    requires io.vertx.core;
-    requires io.vertx.web.client;
-    requires org.jetbrains.annotations;
+    requires transitive io.github.sinri.keel.base;
+    requires transitive io.github.sinri.keel.logger.api;
+    requires transitive io.vertx.core;
+    requires transitive io.vertx.web.client;
+    requires transitive org.jetbrains.annotations;
     requires org.lz4.java;
-    requires io.github.sinri.keel.core;
+    requires transitive io.github.sinri.keel.core;
 
     // Public API packages
     exports io.github.sinri.keel.integration.aliyun.sls;
